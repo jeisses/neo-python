195233b23cc874eccf5a516e73c0e604508e08dd
Jesse Eisses <jesse.eisses@gmail.com>
2018-03-14 08:48:26 +0100

    Fix issue with multisig contracts
    - MultiSig now supports dynamic `m` (this was fixed at 3)
    - Add support for multisig contracts with only 1 required sig

--- neo/SmartContract/Contract.py
+++ neo/SmartContract/Contract.py
@@ -91,7 +91,7 @@ class Contract(SerializableMixin, VerificationCode):
     @staticmethod
     def CreateMultiSigRedeemScript(m, publicKeys):
 
-        if m < 2 or m > len(publicKeys) or len(publicKeys) > 1024:
+        if m < 1 or m > len(publicKeys) or len(publicKeys) > 1024:
             raise Exception('Invalid keys')
 
         sb = ScriptBuilder()
@@ -114,7 +114,7 @@ class Contract(SerializableMixin, VerificationCode):
 
         pk = [ECDSA.decode_secp256r1(p).G for p in publicKeys]
         return Contract(Contract.CreateMultiSigRedeemScript(m, pk),
-                        bytearray(b'\x00\x00\x00'),
+                        bytearray(b'\x00')*m,
                         publicKeyHash)
 
     @staticmethod
